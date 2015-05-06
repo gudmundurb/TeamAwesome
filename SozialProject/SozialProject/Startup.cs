afed0ff033a0145e6b5517d2e24c6ead8c090a0e
Benedikt Hólm Þórðarson <benedikthth@gmail.com>
2015-05-06 13:04:05 +0000

    created a new project again lol

---
+++ SozialProject/SozialProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SozialProject.Startup))]
+namespace SozialProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
