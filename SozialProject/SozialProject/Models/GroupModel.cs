844fe84c45f4c680667f963c8a9b8f5c1790dbca
Benedikt Hólm Þórðarson <benedikthth@gmail.com>
2015-05-06 15:26:54 +0000

    adding moodels

--- SozialProject/SozialProject/Models/GroupModel.cs
+++ SozialProject/SozialProject/Models/GroupModel.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SozialProject.Models
 {
     public class GroupModel
     {
+        [Key]
+        private int groupId;
+
+        [Key]
+        private int creatorId;
+
+
+        [Required]
+        [Display(Name = "Group Name")]
+        public string groupName;
+
+
+        [Required]
+        [Display(Name = "Group Description")]
+        public string groupDescription;
+        
+
     }
 }
--- SozialProject/SozialProject/Models/Post .cs
+++ SozialProject/SozialProject/Models/Post .cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace SozialProject.Models
 {
     public class Post
     {
+        [Key]
+        private int id; //the individual posts' id.
+
+        [Key]
+        private int userID; // the user that made the comment.
+
+        [Required]
+        [Display(Name = "Comment text")]
+        public string commentText;
+
+        
     }
 }
