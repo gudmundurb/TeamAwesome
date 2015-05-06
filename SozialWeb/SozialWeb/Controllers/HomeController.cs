5ba3b16265aac3a436fff157d0e0e4b81976ae14
Benedikt Hólm Þórðarson <benedikthth@gmail.com>
2015-05-06 12:44:01 +0000

    added gitignore file

---
+++ SozialWeb/SozialWeb/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SozialWeb.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			// If you want to display a list of the currently logged
+			// in users friends, you could use the FriendService
+			// to do that.
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ SozialWeb/SozialWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SozialWeb.MvcApplication" Language="C#" %>
